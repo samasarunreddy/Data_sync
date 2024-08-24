bbd67519f6808e5ca7ec531b34410c9c7858c539
samasarunreddy <sarunnanigithub@gmail.com>
2024-08-25 01:05:04 +0530

    feat: added rocket animation with color

--- src/components/ServerProgress.jsx
+++ src/components/ServerProgress.jsx
@@ -1,58 +1,73 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 
 import { useEffect, useState } from 'react';
 
 export default function ServerProgress({ message, instance, percentage }) {
-  const [progress, setProgress] = useState(0);
+  const [size, setSize] = useState(0);
+
+  const radius = `calc(${size}px * 0.405)`;
+  const rocketHeight = `5rem`;
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((prev) => {
-        const newProgress = (prev + 1) % 100;
-        return newProgress;
-      });
-    }, 40);
+    const updateSize = () => {
+      const minDimension = Math.min(window.innerWidth, window.innerHeight);
+      setSize(minDimension);
+    };
+    updateSize();
+    window.addEventListener('resize', updateSize);
 
-    return () => clearInterval(interval);
+    return () => window.removeEventListener('resize', updateSize);
   }, []);
 
   return (
-    <div className='w-2/3 relative'>
-      {/* Circular Background */}
-      <div className='absolute inset-0 flex items-center justify-center'>
-        <div className='w-[50rem] h-[50rem] bg-white rounded-full flex justify-center items-center'>
-          <div className='w-[48rem] h-[48rem] bg-form-bg-color rounded-full absolute flex flex-col justify-center items-center'>
-            <span>{message}</span>
-            <span className='text-3xl'>{percentage}</span>
-            <span className='text-xl pt-2'>{instance}</span>
+    <div className='w-full h-screen flex flex-col justify-center items-center'>
+      <div
+        className='flex justify-center items-center relative'
+        style={{ width: size, height: size }}
+      >
+        <div className='relative z-10 flex justify-between items-center w-full'>
+          <img
+            src='/assets/images/serverLeft.png'
+            alt='server-left'
+            className='relative z-20 h-[calc(100vh-60vh)]'
+          />
+          <img
+            src='/assets/images/serverRight.png'
+            alt='server-right'
+            className='relative z-20 h-[calc(100vh-60vh)]'
+          />
+        </div>
+        <div
+          style={{
+            background: `conic-gradient(
+              #808080 ${percentage * 3.6}deg,
+              #ffffff ${percentage * 3.6}deg 360deg
+            )`,
+            transform: 'rotate(-90deg)',
+            width: '70%',
+            height: '70%',
+          }}
+          className='absolute bg-white w-[70%] h-[70%] rounded-full flex flex-col justify-center p-1'
+        >
+          <div className='flex flex-col items-center justify-center bg-form-bg-color rounded-full w-full h-full rotate-90'>
+            <span>{message ?? 'hi'}</span>
+            <span className='text-3xl'>{percentage}%</span>
+            <span className='text-xl pt-2'>{instance ?? '100/100'}</span>
           </div>
         </div>
-      </div>
-
-      {/* Images */}
-      <div
-        className='relative z-10 flex justify-between items-center w-full'
-        style={{ '--progress': `${progress}%` }}
-      >
-        <img
-          src='/assets/images/serverLeft.png'
-          alt='server-left'
-          className='relative z-20'
-        />
-        <img
-          src='/assets/images/serverRight.png'
-          alt='server-right'
-          className='relative z-20'
-        />
-      </div>
-
-      {/* Rocket Animation */}
-      <div className='absolute inset-0 flex items-center justify-center'>
-        <div className='rocket-container'>
+        <div
+          className='absolute'
+          style={{
+            transform: `rotate(${
+              90 + (percentage * 360) / 100
+            }deg) translateY(calc(${radius} - (${rocketHeight} / 2)))`,
+          }}
+        >
           <img
             src='/assets/images/rocket.png'
             alt='rocket'
-            className='w-[9rem] rocket-image'
+            className='w-[5rem] rotate-[270deg]'
           />
         </div>
       </div>
